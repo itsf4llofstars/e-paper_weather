9f7f7db032de011c3adfd2c77b3a9b9d2261e205
itsf4llofstars <irooted4hal@mailfence.com>
2023-06-05 15:49:43 -0500

    Change Himage to metar, remvoe icon prints

--- src/main.py
+++ src/main.py
@@ -49,22 +49,11 @@ def main():
         font18 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 18)
         font35 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 35)
 
-        # Drawing on the Horizontal image
-        logging.info("1.Drawing on the Horizontal image...")
-        Himage = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
-        draw = ImageDraw.Draw(Himage)
+        # Draw the METAR weather
+        metar_wx = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(metar_wx)
         draw.text((10, 0), "hello world", font=font24, fill=0)
-        draw.text((10, 20), "4.2inch e-Paper", font=font24, fill=0)
-        draw.text((150, 0), "微雪电子", font=font24, fill=0)
-        draw.line((20, 50, 70, 100), fill=0)
-        draw.line((70, 50, 20, 100), fill=0)
-        draw.rectangle((20, 50, 70, 100), outline=0)
-        draw.line((165, 50, 165, 100), fill=0)
-        draw.line((140, 75, 190, 75), fill=0)
-        draw.arc((140, 50, 190, 100), 0, 360, fill=0)
-        draw.rectangle((80, 50, 130, 100), fill=0)
-        draw.chord((200, 50, 250, 100), 0, 360, fill=0)
-        epd.display(epd.getbuffer(Himage))
+        epd.display(epd.getbuffer(metar_wx))
         time.sleep(2)
 
         epd.Clear()
