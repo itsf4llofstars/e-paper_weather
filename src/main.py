5d4fa655fa03f1195ec426b8369a6b723a29384b
itsf4llofstars <irooted4hal@mailfence.com>
2023-06-05 15:28:36 -0500

    Add and setup src/main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+File: main.py
+Author: itsf4llofstarts
+Description: Weather text and ascii displayd on e-paper
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
