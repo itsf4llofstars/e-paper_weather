dc7844ca22cded62357bca5884dd451a2b874224
itsf4llofstars <irooted4hal@mailfence.com>
2023-06-05 15:39:47 -0500

    Add imports and try: import except importerrro

--- src/main.py
+++ src/main.py
@@ -4,10 +4,33 @@ File: main.py
 Author: itsf4llofstarts
 Description: Weather text and ascii displayd on e-paper
 """
+import logging
+import os
+import sys
+import time
+import traceback
+
+from PIL import Image, ImageDraw, ImageFont
+
+picdir = ""
+libdir = ""
+
+try:
+    picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+    libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+
+    if os.path.exists(libdir):
+        sys.path.append(libdir)
+
+    from waveshare_epd import epd4in2
+
+    logging.basicConfig(level=logging.DEBUG)
+except ImportError as ie:
+    sys.exit()
+
 
 
 def main():
-    pass
 
 
 if __name__ == "__main__":
