8dce76d982c3cc23d75cf98ca7532e514f75a3de
itsf4llofstars <irooted4hal@mailfence.com>
2023-06-05 15:46:47 -0500

    Add initial display test print

--- src/main.py
+++ src/main.py
@@ -37,43 +37,44 @@ else:
 
 
 def main():
-    logging.info("epd4in2 Demo")
+    try:
+        logging.info("epd4in2 Demo")
 
-    epd = epd4in2.EPD()
-    logging.info("init and Clear")
-    epd.init()
-    epd.Clear()
+        epd = epd4in2.EPD()
+        logging.info("init and Clear")
+        epd.init()
+        epd.Clear()
 
-    font24 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 18)
-    font35 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 35)
+        font24 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 24)
+        font18 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 18)
+        font35 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 35)
 
-    # Drawing on the Horizontal image
-    logging.info("1.Drawing on the Horizontal image...")
-    Himage = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(Himage)
-    draw.text((10, 0), "hello world", font=font24, fill=0)
-    draw.text((10, 20), "4.2inch e-Paper", font=font24, fill=0)
-    draw.text((150, 0), "微雪电子", font=font24, fill=0)
-    draw.line((20, 50, 70, 100), fill=0)
-    draw.line((70, 50, 20, 100), fill=0)
-    draw.rectangle((20, 50, 70, 100), outline=0)
-    draw.line((165, 50, 165, 100), fill=0)
-    draw.line((140, 75, 190, 75), fill=0)
-    draw.arc((140, 50, 190, 100), 0, 360, fill=0)
-    draw.rectangle((80, 50, 130, 100), fill=0)
-    draw.chord((200, 50, 250, 100), 0, 360, fill=0)
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
+        # Drawing on the Horizontal image
+        logging.info("1.Drawing on the Horizontal image...")
+        Himage = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(Himage)
+        draw.text((10, 0), "hello world", font=font24, fill=0)
+        draw.text((10, 20), "4.2inch e-Paper", font=font24, fill=0)
+        draw.text((150, 0), "微雪电子", font=font24, fill=0)
+        draw.line((20, 50, 70, 100), fill=0)
+        draw.line((70, 50, 20, 100), fill=0)
+        draw.rectangle((20, 50, 70, 100), outline=0)
+        draw.line((165, 50, 165, 100), fill=0)
+        draw.line((140, 75, 190, 75), fill=0)
+        draw.arc((140, 50, 190, 100), 0, 360, fill=0)
+        draw.rectangle((80, 50, 130, 100), fill=0)
+        draw.chord((200, 50, 250, 100), 0, 360, fill=0)
+        epd.display(epd.getbuffer(Himage))
+        time.sleep(2)
 
-    epd.Clear()
-    epd.sleep()
-except IOError as e:
-    logging.info(e)
-except KeyboardInterrupt:
-    logging.info("ctrl + c:")
-    epd4in2.epdconfig.module_exit()
-    exit()
+        epd.Clear()
+        epd.sleep()
+    except IOError as e:
+        logging.info(e)
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd4in2.epdconfig.module_exit()
+        exit()
 
 
 if __name__ == "__main__":
