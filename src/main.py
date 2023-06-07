4fc5f3c9c196114ba60817c32598e29af6bf79e6
itsf4llofstars <irooted4hal@mailfence.com>
2023-06-07 01:32:39 -0500

    Add set lat and long to lat lon variables

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ Description: Weather text and ascii displayd on e-paper
 """
 import logging
 import os
+import re
 import sys
 import time
 import traceback
@@ -26,41 +27,54 @@ from waveshare_epd import epd4in2
 
 logging.basicConfig(level=logging.DEBUG)
 
+iss_json_file = os.path.expanduser(os.path.join("~", "python", "iss_track", "iss.json"))
 
-def main():
-    try:
-        epd = epd4in2.EPD()
+file_lines = []
+with open(iss_json_file) as read:
+    file_lines = read.readlines()
 
-        epd.init()
-        epd.Clear()
-        if len(sys.argv) == 2 and sys.argv[1] == "-c":
-            epd.sleep()
-            sys.exit()
+iss_json_posit = file_lines[-1]
+del file_lines
 
-        font18 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 18)
-        font20 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 20)
-        font22 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 22)
-        font24 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 24)
-        font36 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 36)
+if "success" not in iss_json_posit:
+    sys.exit()
 
-        lat = "48.20.09"
-        lon = "122.39.07"
-        iss = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
-        draw = ImageDraw.Draw(iss)
-        draw.text((10, 0), "ISS", font=font36, fill=0)
-        draw.text((10, 40), "Latitude / Longitude", font=font36, fill=0)
-        draw.text((10, 80), f"{lat}, {lon}", font=font36, fill=0)
-        epd.display(epd.getbuffer(iss))
+print(iss_json_posit)
 
-        # epd.Clear()
+posit = re.compile(r"-?\d{1,3}\.\d{1,4}")
+
+position = re.findall(posit, iss_json_posit)
+lat = position[0]
+lon = position[1]
+
+try:
+    epd = epd4in2.EPD()
+
+    epd.init()
+    epd.Clear()
+
+    if len(sys.argv) == 2 and sys.argv[1] == "-c":
         epd.sleep()
-    except IOError as e:
-        logging.info(e)
-    except KeyboardInterrupt:
-        logging.info("ctrl + c:")
-        epd4in2.epdconfig.module_exit()
-        exit()
+        sys.exit()
 
+    font18 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 18)
+    font20 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 20)
+    font22 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 22)
+    font24 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 24)
+    font36 = ImageFont.truetype(os.path.join(picdir, "Font.ttc"), 36)
 
-if __name__ == "__main__":
-    sys.exit(main())
+    iss = Image.new("1", (epd.width, epd.height), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(iss)
+    draw.text((10, 0), "ISS", font=font36, fill=0)
+    draw.text((10, 40), "Latitude / Longitude", font=font36, fill=0)
+    draw.text((10, 80), f"{lat}, {lon}", font=font36, fill=0)
+    epd.display(epd.getbuffer(iss))
+
+    # epd.Clear()
+    epd.sleep()
+except IOError as e:
+    logging.info(e)
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    epd4in2.epdconfig.module_exit()
+    exit()
